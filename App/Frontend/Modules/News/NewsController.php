ca949840fac35094c9b7c02f79aafba0e4ffb9e1
Malamine <malaminedieng7@gmail.com>
2021-02-02 20:09:16 +0100

    Gestion des formulaires

--- App/Frontend/Modules/News/NewsController.php
+++ App/Frontend/Modules/News/NewsController.php
@@ -5,10 +5,52 @@ use \OCFram\BackController;
 use \OCFram\HTTPRequest;
 use \Entity\News;
 use \Entity\Comment;
+use \OCFram\Form;
+use \OCFram\StringField;
+use \OCFram\TextField;
+
 
 class NewsController extends BackController
 {
-  // ...
+  public function executeInsertComment(HTTPRequest $request)
+  {
+    // Si le formulaire a été envoyé, on crée le commentaire avec les valeurs du formulaire.
+    if ($request->method() == 'POST')
+    {
+      $comment = new Comment([
+        'news' => $request->getData('news'),
+        'auteur' => $request->postData('auteur'),
+        'contenu' => $request->postData('contenu')
+      ]);
+    }
+    else
+    {
+      $comment = new Comment;
+    }
+    
+    $form = new Form($comment);
+    
+    $form->add(new StringField([
+        'label' => 'Auteur',
+        'name' => 'auteur',
+        'maxLength' => 50,
+       ]))
+       ->add(new TextField([
+        'label' => 'Contenu',
+        'name' => 'contenu',
+        'rows' => 7,
+        'cols' => 50,
+       ]));
+    
+    if ($form->isValid())
+    {
+      // On enregistre le commentaire
+    }
+    
+    $this->page->addVar('comment', $comment);
+    $this->page->addVar('form', $form->createView()); // On passe le formulaire généré à la vue.
+    $this->page->addVar('title', 'Ajout d\'un commentaire');
+  }
   
   public function executeShow(HTTPRequest $request)
   {
